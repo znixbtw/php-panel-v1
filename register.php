5c4cf61c1bbecb32391072edb5559d1d45e2fb98
znix <64723880+znixhook@users.noreply.github.com>
2020-08-14 09:12:14 +0530

    Fixed XSS

--- register.php
+++ register.php
@@ -150,23 +150,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                     <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
                         <label>Username</label>
-                        <input type="text" name="username" class="form-control form-control-sm" value="<?php echo $username; ?>">
-                        <span class="help-block"><?php echo $username_err; ?></span>
+                        <input type="text" name="username" class="form-control form-control-sm">
+                        <span class="help-block"><?php echo htmlspecialchars($username_err); ?></span>
                     </div>
                     <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
                         <label>Password</label>
-                        <input type="password" name="password" class="form-control form-control-sm" value="<?php echo $password; ?>">
-                        <span class="help-block"><?php echo $password_err; ?></span>
+                        <input type="password" name="password" class="form-control form-control-sm">
+                        <span class="help-block"><?php echo htmlspecialchars($password_err); ?></span>
                     </div>
                     <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
                         <label>Confirm Password</label>
-                        <input type="password" name="confirm_password" class="form-control form-control-sm" value="<?php echo $confirm_password; ?>">
-                        <span class="help-block"><?php echo $confirm_password_err; ?></span>
+                        <input type="password" name="confirm_password" class="form-control form-control-sm">
+                        <span class="help-block"><?php echo htmlspecialchars($confirm_password_err); ?></span>
                     </div>
                     <div class="form-group <?php echo (!empty($invite_code_err)) ? 'has-error' : ''; ?>">
                         <label>Invite Code</label>
-                        <input type="text" name="invite_code" class="form-control form-control-sm" value="<?php echo $invite_code; ?>">
-                        <span class="help-block"><?php echo $invite_code_err; ?></span>
+                        <input type="text" name="invite_code" class="form-control form-control-sm">
+                        <span class="help-block"><?php echo htmlspecialchars($invite_code_err); ?></span>
                     </div>
                     <div class="form-group text-center">
                         <input type="submit" class="btn btn-custom px-2 mx-1 w-auto" value="Register">
