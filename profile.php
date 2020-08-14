e3fbf81f54767c8f02fae4fe3bee2f7c391f95a6
znix <64723880+znixhook@users.noreply.github.com>
2020-08-14 10:21:20 +0530

    Fixed XSS

--- profile.php
+++ profile.php
@@ -102,13 +102,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                             <div class="form-group <?php echo (!empty($new_password_err)) ? 'has-error' : ''; ?>">
                                 <label>New Password</label>
-                                <input type="password" name="new_password" class="form-control form-control-sm" value="<?php echo $new_password; ?>">
-                                <span class="help-block"><?php echo $new_password_err; ?></span>
+                                <input type="password" name="new_password" class="form-control form-control-sm">
+                                <span class="help-block"><?php echo htmlspecialchars($new_password_err); ?></span>
                             </div>
                             <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
                                 <label>Confirm Password</label>
                                 <input type="password" name="confirm_password" class="form-control form-control-sm">
-                                <span class="help-block"><?php echo $confirm_password_err; ?></span>
+                                <span class="help-block"><?php echo htmlspecialchars($confirm_password_err); ?></span>
                             </div>
                             <div class="form-group">
                                 <input type="submit" class="btn btn-primary" value="Submit">
