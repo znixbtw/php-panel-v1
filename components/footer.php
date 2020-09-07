ff3bacca1d1f158a7a07cee62f28bc8cf6cc15fb
znix <64723880+znixhook@users.noreply.github.com>
2020-09-07 14:44:10 +0530

    removed loader animation

--- components/footer.php
+++ components/footer.php
@@ -8,19 +8,9 @@
         </div>
     </div>
 </footer>
-<div class="loader-wrapper">
-    <span class="loader"><span class="loader-inner"></span></span>
-</div>
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="http://code.jquery.com/jquery-latest.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
 <script src="https://use.fontawesome.com/4c8739c9b7.js"></script>
-
-
-<script>
-$(window).on("load",function(){
-    $(".loader-wrapper").fadeOut("slow");
-});
-</script>
